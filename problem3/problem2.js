513b24dc5a8bddf956e4e0dfcc74bde410b0e0cf
mstsurnalyakter <surnalyms@gmail.com>
2024-02-03 15:25:31 +0600

    first half string

---
+++ problem3/problem2.js
@@ -0,0 +1,7 @@
+/**
+ * write a JavaScript program to extract the first half of a string of even length;
+ */
+
+const firstHalf = str => str.slice(0, str.length/2)
+
+console.log(firstHalf("Surnaly"));
