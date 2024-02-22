22adeefa8e7a8a0b0a870cac5067bbd073e19738
mstsurnalyakter <surnalyms@gmail.com>
2024-02-22 19:44:10 +0600

    reduce

---
+++ problem5/reduce.js
@@ -0,0 +1,8 @@
+const numbers = [74, 44, 23, 74, 33];
+
+const sum = numbers.reduce((previous,current)=> previous + current,0);
+console.log(sum);
+
+const total = numbers.reduce((previous,current)=> previous + current,0);
+
+console.log(total);
