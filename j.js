4bb187401c1e343866a45f6eac1b46ba05209235
mstsurnalyakter <surnalyms@gmail.com>
2024-02-22 18:49:40 +0600

    Optional chaining

---
+++ j.js
@@ -0,0 +1,12 @@
+/** Optional chaining */
+
+const users = {
+    id:400,
+    name:'jkldddddk',
+    addres:{
+        city:'dhaka',
+        country:'bangladesh'
+    }
+}
+
+console.log(users.addres.streest?.second);
